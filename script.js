cf5a9a045a6966912e1c709d541de6fced9070e4
Taranjot <taranjotdhaliwal07@gmail.com>
2022-03-31 16:06:41 -0500

    changedTheAPIsAspreviousWereNotWorking

--- script.js
+++ script.js
@@ -2,50 +2,43 @@ var xhttp = new XMLHttpRequest();
 document.getElementById("myBtn").addEventListener("click", function(){
    var demo= document.getElementById("demo")
    demo.innerHTML="";
-    var select = document.getElementById("categories");
-    var value = select.options[select.selectedIndex].value;
-      console.log(value);
-      var quantity = document.getElementById("quantity");
-      var number = quantity.value;
-      console.log(number);
+    var language = document.getElementById("language");
+    var lValue = language.options[language.selectedIndex].value;
+    lValue=lValue.toLowerCase();
+      console.log(lValue);
+    
+    var category = document.getElementById("category");
+    var cValue = category.options[category.selectedIndex].value;
+    cValue = cValue.toLowerCase();
+      console.log(cValue);
+
+
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-      // document.getElementById("demo").innerHTML ;
+       
        var Janswer= xhttp.responseText;
        console.log(Janswer);
-      var answer= JSON.parse(Janswer);
-       for(var i=0; i<answer.length;i++){
-        const para = document.createElement("li");
+     var answer= JSON.parse(Janswer);
+     console.log(answer.text);
+       const para = document.createElement("p");
         para.setAttribute("id", "desc")
-       
-        var excuse= answer[i].excuse; 
-        if(excuse==undefined){
-            excuse= answer[i].death
-        }
-        para.innerText =excuse       
+        para.innerText =answer.text      
         demo.appendChild(para);
-       }
+       
     }
 };
-// will edit API
-if(value=="any"){
-    if(number){
-        xhttp.open("GET", "https://excuser.herokuapp.com/v1/excuse/"+number);  
-    }else{
-xhttp.open("GET", "https://excuser.herokuapp.com/v1/excuse");
-    }
+if(lValue==="english"){
+    console.log("hello english");
+    xhttp.open("GET", "https://uselessfacts.jsph.pl/"+ cValue+ ".json?language=en");
+}else{
+if(lValue==="german"){
+    xhttp.open("GET", "https://uselessfacts.jsph.pl/"+ cValue+ ".json?language=de");
+}
+else{
+    xhttp.open("GET", "https://uselessfacts.jsph.pl/"+ cValue+ ".json");
+}
+}
 
-}
-else
-{
-    if(number){
-        xhttp.open("GET", "https://excuser.herokuapp.com/v1/excuse/"+value+"/"+number);  
-    }else{
-    xhttp.open("GET", "https://excuser.herokuapp.com/v1/excuse/"+value);
-    }
-    
-}
 
 xhttp.send();
 
